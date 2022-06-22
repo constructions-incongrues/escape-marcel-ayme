92aa84f5f0ed4197f4aa2054984f4666858f961a
Tristan Rivoallan <tristan@rivoallan.net>
2022-06-22 20:33:10 +0200

    smoke sound

--- Scripts/smoke.js
+++ Scripts/smoke.js
@@ -3,6 +3,10 @@
 
 
     $(window).on('click', function () {
+      var lab = new Howl({
+        src: ['Sounds/laboratory.mp3'],
+    });
+    lab.play();
       $('.js-smoke_smoke').toggleClass('js-ag-smoke_toggle');
     });
 
